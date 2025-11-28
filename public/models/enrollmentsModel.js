9d374f7c88da31ce0ef9aaa8af79ba8e1aa9e8ad
herben <heredibenedek@gmail.com>
2025-11-28 01:08:25 +0100

    feat: updateEnrollment & errorAt: createEnrollment

--- public/models/enrollmentsModel.js
+++ public/models/enrollmentsModel.js
@@ -9,3 +9,16 @@ export const getEnrollmentById = async (id) => {
   const [rows] = await pool.execute('SELECT * FROM enrollments WHERE id = ?', [id]);
   return rows[0];
 }
+
+export const createEnrollment = async (studentId, courseId) => {
+  const [result] = await pool.execute(
+    'INSERT INTO enrollments (student_id, course_id, enrolled_at) VALUES (?, ?, ?)', [studentId, courseId, Now()]); //Valamiért nem működik a Now() függvény, az előző feladatokban működött...
+  return result.insertId;
+}
+
+export const updateEnrollment = async (id, studentId, courseId) => {
+  await pool.execute(
+    'UPDATE enrollments SET student_id = ?, course_id = ? WHERE id = ?',
+    [studentId, courseId, id]
+  );
+}
--- public/routers/enrollmentsRouter.js
+++ public/routers/enrollmentsRouter.js
@@ -13,7 +13,7 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  
+
     if(id < 0){
         return res.status(404).send({ error: 'Az ID nem lehet negatív!' });
     }
@@ -28,6 +28,41 @@ router.get('/:id', async (req, res) => {
   } catch (error) {
     res.status(501).send({ error: 'Nem lehetett lekérdezni a beiratkozást!' });
   }
+});
+
+router.post('/', async (req, res) => {
+  const { studentId, courseId, enrolled_at } = req.body;
+
+    if (!studentId || !courseId) {
+        return res.status(400).send({ error: 'Hiányzó adatok!' });
+    }
+
+    try {
+      const newEnrollmentId = await enrollmentsModel.createEnrollment(studentId, courseId, enrolled_at);
+      res.status(201).send({ id: newEnrollmentId });
+    } catch (error) {
+      res.status(501).send({ error: 'Nem lehetett létrehozni a beiratkozást!' , details: error.message});
+    }
+});
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { studentId, courseId } = req.body;
+
+    if(id < 0){
+        return res.status(404).send({ error: 'Az ID nem lehet negatív!' });
+    }
+
+    if (!studentId || !courseId) {
+        return res.status(400).send({ error: 'Hiányzó adatok!' });
+    }
+
+    try {
+      await enrollmentsModel.updateEnrollment(id, studentId, courseId);
+      res.status(200).send({ message: 'Beiratkozás frissítve!' });
+    } catch (error) {
+      res.status(501).send({ error: 'Nem lehetett frissíteni a beiratkozást!', details: error.message });
+    }
 });
 
 export default router;
